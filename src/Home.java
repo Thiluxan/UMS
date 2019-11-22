64289521819997016a56f805a096a61bd3b9e27b
Thiluxan <48411475+Thiluxan@users.noreply.github.com>
2019-11-22 17:01:35 +0530

    Update Home.java

--- src/Home.java
+++ src/Home.java
@@ -1,22 +1,40 @@
+package lab;
+import java.sql.*;
 import java.util.Scanner;
 public class Home {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to University Management System");
-		System.out.println("Press [1] Register as a student");
-		System.out.println("Press [2] Register as a lecturer");
-		Scanner sc = new Scanner(System.in);
-		int selection = sc.nextInt();
-		StudentRegistration stu = new StudentRegistration();
-		switch(selection) {
-		case 1:
-			stu.input();
-			stu.Display();
-			break;
-		case 2:
-			System.out.println("System under construction");
-			break;
-		}
-	}
+   public static void main(String[] args) {
+
+      System.out.println("Welcome to University Management System");
+      System.out.println("Select, [1] Sign Up [2] Sign in");
+      Scanner sc = new Scanner(System.in);
+      int selection = sc.nextInt();
+      System.out.println("Please select your registration role [1] leccturer [2] Student");
+      int s1 = sc.nextInt();
+      Student_Registration stu = new Student_Registration();
+      Student_Login SL = new Student_Login();
+      switch(selection) {
+         case 1:
+            if(s1 == 2){
+                stu.input();
+                stu.add();
+                stu.Display();
+                }
+                else if(s1 == 1){
+                System.out.println("System under construction");
+                }
+            break;
+         case 2:
+            if(s1==2){
+                SL.input();
+                SL.retrieve();
+                SL.choice();
+                }
+                else if(s1 == 1){
+                System.out.println("System under construction");
+                }
+            break;
+      }
+   }
 
 }
